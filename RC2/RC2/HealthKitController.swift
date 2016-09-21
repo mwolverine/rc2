d8ba63bb45c7edf8d9b62581741b3ea3d5dd12fb
Ryan Plitt <ryanplitt@gmail.com>
2016-09-21 10:22:43 -0600

    updated plist and capabilities for healthkit

---
+++ RC2/RC2/HealthKitController.swift
@@ -0,0 +1,49 @@
+//
+//  HealthKitController.swift
+//  RC2
+//
+//  Created by Ryan Plitt on 9/21/16.
+//  Copyright © 2016 Chris Yoo. All rights reserved.
+//
+
+import Foundation
+import HealthKit
+
+class HealthKitController {
+    
+    func authorizeHealthKit(completio: ((success: Bool, error: NSError!) -> Void)?) {
+        
+        
+        
+    }
+    
+    
+    func setupQueryForQuantityType(queryResultsHandler: (HKObserverQuery, HKObserverQueryCompletionHandler, NSError?) -> Void) {
+        
+        
+        
+        
+    }
+    
+    func enableBackgroundDelivery(){
+        
+        
+        
+    }
+    
+    
+    func backgroundQueryHandler(query: HKObserverQuery, completionHandler: HKObserverQueryCompletionHandler, error: NSError?) {
+        
+        
+        
+    }
+    
+    
+    func backgroundResultsHandler(query: HKObserverQuery, results: String = "This is not right. Needs to be results Type", error: NSError?) {
+        
+        
+        
+        
+    }
+    
+}
