f7501f0ea5fb92c833dba22426c9eebe7bbe9ce3
Chris Yoo <ncyoo@outlook.com>
2016-09-21 13:44:46 -0600

    changes

--- RC2/RC2/FacebookController.swift
+++ RC2/RC2/FacebookController.swift
@@ -9,12 +9,75 @@
 import Foundation
 import FBSDKLoginKit
 import FBSDKCoreKit
+import Firebase
+import FirebaseAuth
 
 class FacebookController {
     
     var friendData: [String] = []
     
     static let sharedController = FacebookController()
+    
+    func facebookCredential() {
+        //FIREBASE: Integration with Firebase through authentication
+        
+        let credential = FIRFacebookAuthProvider.credentialWithAccessToken(FBSDKAccessToken.currentAccessToken().tokenString)
+        
+        FIRAuth.auth()?.signInWithCredential(credential) { (user, error) in
+            if let error = error {
+                print(error)
+            } else {
+                
+                self.signedIn(user)
+                
+                print("Firebase authenticated")
+                
+                //FIREBASE: Create a user in database with the same authentication UID
+                
+                FIRAuth.auth()?.addAuthStateDidChangeListener({ (auth, user) in
+                    let firebaseURL = FIRDatabase.database().referenceFromURL("https://rc2p-15dd8.firebaseio.com/")
+                    
+                    if (user != nil) {
+                        
+                        guard let user = user else { return }
+                        guard let photoLink = user.photoURL else { return }
+                        let photoStringURL = photoLink.absoluteString
+                        
+                        let newUser = ["provider": user.providerID as String, "displayName": user.displayName! as String, "email": user.email! as String, "photoURL": photoStringURL as String]
+                        self.uid = user.uid as String
+                        let usersReference = firebaseURL.child("users/\(self.uid)")
+                        
+                        usersReference.child("UserInfo").updateChildValues(newUser) { (err, ref) in
+                            if err != nil {
+                                print(err)
+                                return
+                            }
+                            print("saved successful in firebase database")
+                        }
+                        self.returnUserDataButtonTapped(self)
+                        
+                    }
+                })
+            }
+        }
+        
+    }
+    
+    func createFriend(friendID: String, friendName: String) {
+        let firebaseURL = FIRDatabase.database().referenceFromURL("https://rc2p-15dd8.firebaseio.com/")
+        let userssReference = firebaseURL.child("users/\(uid)")
+        
+        let friendsInfo = [friendID: friendName]
+        
+        userssReference.child("FriendList").updateChildValues(friendsInfo, withCompletionBlock: { (err, ref) in
+            if err != nil {
+                print(err)
+                return
+            }
+        })
+    }
+    
+    
     
     func returnUserData() {
         let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "/me/friends", parameters: nil)
--- RC2/RC2/LoginViewController.swift
+++ RC2/RC2/LoginViewController.swift
@@ -54,63 +54,11 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
             return
         }
         print("User logged in")
-        //FIREBASE: Integration with Firebase through authentication
         
-        let credential = FIRFacebookAuthProvider.credentialWithAccessToken(FBSDKAccessToken.currentAccessToken().tokenString)
-        
-        FIRAuth.auth()?.signInWithCredential(credential) { (user, error) in
-            if let error = error {
-                print(error)
-            } else {
-                
-                self.signedIn(user)
-                
-                print("Firebase authenticated")
-                
-                //FIREBASE: Create a user in database with the same authentication UID
-                
-                FIRAuth.auth()?.addAuthStateDidChangeListener({ (auth, user) in
-                    let firebaseURL = FIRDatabase.database().referenceFromURL("https://rc2p-15dd8.firebaseio.com/")
-                    
-                    if (user != nil) {
-                        
-                        guard let user = user else { return }
-                        guard let photoLink = user.photoURL else { return }
-                        let photoStringURL = photoLink.absoluteString
-                        
-                        let newUser = ["provider": user.providerID as String, "displayName": user.displayName! as String, "email": user.email! as String, "photoURL": photoStringURL as String]
-                         self.uid = user.uid as String
-                        let usersReference = firebaseURL.child("users/\(self.uid)")
-                        
-                        usersReference.child("UserInfo").updateChildValues(newUser) { (err, ref) in
-                            if err != nil {
-                                print(err)
-                                return
-                            }
-                            print("saved successful in firebase database")
-                        }
-                        
-                      
-                    }
-                })
-            }
-        }
+        FacebookController.sharedController.facebookCredential()
     }
     
-    func createFriend(friendID: String, friendName: String) {
-        let firebaseURL = FIRDatabase.database().referenceFromURL("https://rc2p-15dd8.firebaseio.com/")
-        let userssReference = firebaseURL.child("users/\(uid)")
-        
-        let friendsInfo = [friendID: friendName]
-
-        userssReference.child("FriendList").updateChildValues(friendsInfo, withCompletionBlock: { (err, ref) in
-            if err != nil {
-                print(err)
-                return
-            }
-        })
-    }
-    
+ 
     func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
         print("User Logged Out")
     }
@@ -118,7 +66,7 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
     @IBAction func returnUserDataButtonTapped(sender: AnyObject) {
         FacebookController.sharedController.returnUserData()
     }
-    //COPY    
+    //COPY
     
     func signedIn(user: FIRUser?) {
         //        MeasurementHelper.sendLoginEvent()
